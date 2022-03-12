f623c447a1e209cccb6d2a73e2450bf6f1b3222c
jtc1246 <43341500+jtc1246@users.noreply.github.com>
2022-03-12 11:14:01 +0800

    Add files via upload

--- myHttp/main.py
+++ myHttp/main.py
@@ -30,7 +30,7 @@ def testInternet():
         return -1
     return 0
 
-def http(url:str,Method:str="GET",Header:dict={},Timeout:int=None,ToJson:bool=True,Body:str="",Decode:bool=True):
+def http(url:str,Method:str="GET",Header:dict={},Timeout:int=None,ToJson:bool=True,Body:str="",Decode:bool=True,Retry:bool=True,ForceHttps:bool=False):
     '''
     About Timeout / 关于 Timeout:
         Default timeout is 2000ms.
@@ -43,6 +43,7 @@ def http(url:str,Method:str="GET",Header:dict={},Timeout:int=None,ToJson:bool=Tr
         -2: 超时 / Timeout
         -3: 域名不存在 / Domian not exists
         -4: 其它问题, 主要是代理服务器设置错误 / Other problem, mainly proxy server error
+        -5: 对方网站不支持 https, 但强制要求 https 链接 / The website does not support https, but you force https connection.
 
         1: 不是 UTF-8 编码, text 返回空字符串 / Not UTF-8 encode, the return value of text will be empty str
         2: 不是 Json 格式(在 toJSON=True 的前提下) / Not in Json format (when toJSON=True)
@@ -50,7 +51,7 @@ def http(url:str,Method:str="GET",Header:dict={},Timeout:int=None,ToJson:bool=Tr
         The website does not support https. It will change to http automatically in this situation. If the status this time is 0, the status will be 3. If not 0, it will return this status.
     '''
     global defaultTimeOut
-    backup=[url,Method,Header,Timeout,ToJson,Body,Decode]
+    backup=[url,Method,Header,Timeout,ToJson,Body,Decode,Retry]
     if(Timeout==None):
         Timeout=defaultTimeOut
     http1=urllib3.PoolManager()
@@ -65,30 +66,36 @@ def http(url:str,Method:str="GET",Header:dict={},Timeout:int=None,ToJson:bool=Tr
         if(testInternet()==-1): # 无网络
             return {'status':-1,'code':0,'text':text,'header':{},'extra':''}
         return {'status':-3,'code':0,'text':text,'header':{},'extra':''}
-    except exceptions.ConnectTimeoutError as err: # 无网络/超时
+    except exceptions.ConnectTimeoutError: # 无网络/超时
         if(testInternet()==-1): # 无网络
             return {'status':-1,'code':0,'text':text,'header':{},'extra':''}
         # 超时
+        if(Retry==False):
+            return {'status':-2,'code':0,'text':text,'header':{},'extra':''}
         try:
             r=http1.request(Method,url,headers=Header,timeout=3*Timeout/1000,body=Body,retries=False)
         except:
             return {'status':-2,'code':0,'text':text,'header':{},'extra':''}
-    except exceptions.ReadTimeoutError as err: # 无网络/超时
+    except exceptions.ReadTimeoutError: # 无网络/超时
         if(testInternet()==-1): # 无网络
             return {'status':-1,'code':0,'text':text,'header':{},'extra':''}
         # 超时
+        if(Retry==False):
+            return {'status':-2,'code':0,'text':text,'header':{},'extra':''}
         try:
             r=http1.request(Method,url,headers=Header,timeout=3*Timeout/1000,body=Body,retries=False)
         except:
             return {'status':-2,'code':0,'text':text,'header':{},'extra':''}
     except exceptions.SSLError: # 对方网站不支持https, 但是却使用了https连接
+        if(ForceHttps):
+            return {'status':-5,'code':0,'text':text,'header':{},'extra':''}
         n=backup[0].find('https://')
         newUrl='http://'+backup[0][n+8:]
-        tryAgain=http(newUrl,Method=backup[1],Header=backup[2],Timeout=backup[3],ToJson=backup[4],Body=backup[5],Decode=backup[6])
+        tryAgain=http(newUrl,Method=backup[1],Header=backup[2],Timeout=backup[3],ToJson=backup[4],Body=backup[5],Decode=backup[6],Retry=backup[7])
         if(tryAgain['status']==0):
             tryAgain['status']=3
         return tryAgain
-    except Exception as err: # 其它错误，主要为代理服务器设置错误，服务器上一般无此问题
+    except Exception: # 其它错误，主要为代理服务器设置错误，服务器上一般无此问题
         return {'status':-4,'code':0,'text':text,'header':{},'extra':''}
     # 以下是正常情况
     resp={}
